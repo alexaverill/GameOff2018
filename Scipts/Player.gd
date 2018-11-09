5ee3f77ac7d11fc152d075ec79dc2b7f6aa98d18
Alex <alex.averill2013@gmail.com>
2018-11-08 20:38:07 -0700

    Addding in new Sprites and starting some work on a basic UI system and playing with themes

--- Scipts/Player.gd
+++ Scipts/Player.gd
@@ -6,7 +6,19 @@ export (int) var gravity = 1200
 
 var velocity = Vector2()
 var jumping = false
+var area
+var otherNode 
+func _ready():
+	area = get_node("Area2D")
+	area.connect("area_entered",self,"handleAreaEntered")
 
+func handleAreaEntered(other):
+	otherNode = get_node(get_path_to(other))
+	if otherNode.is_in_group("enemies"):
+		print("enemies")
+	if otherNode.is_in_group("ui"):
+		print("ui")
+	print("entered area")
 func get_input():
     velocity.x = 0
     var right = Input.is_action_pressed('ui_right')
@@ -18,7 +30,7 @@ func get_input():
         velocity.y = jump_speed
     if right:
         velocity.x += run_speed
-    if left:
+    if left and position.x > -300:
         velocity.x -= run_speed
 
 func _physics_process(delta):
